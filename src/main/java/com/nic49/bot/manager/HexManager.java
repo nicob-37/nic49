315cbda18fa0e7f2afb5e99b07c4d8a6a104e50c
Nico <152223501+nicobota@users.noreply.github.com>
2026-08-06 14:32:08 -0700

    WIP HexManager.java

--- src/main/java/com/nic49/bot/manager/HexManager.java
+++ src/main/java/com/nic49/bot/manager/HexManager.java
@@ -8,27 +8,41 @@ import java.awt.*;
 
 public class HexManager extends ListenerAdapter {
 
+    public class ColorComparison {
+        int abs;
+        double cie76, cie2000;
+
+        public ColorComparison(String hex1, String hex2) {
+            this.cie76 = HexManager.calculateDeltaE76(hex1, hex2);
+            this.abs = HexManager.calculateABS(hex1, hex2);
+        }
+    }
+
     @Override
     public void onReady(@NotNull ReadyEvent event) {
 
     }
 
-
-    public double calculateDeltaE(String hex1, String hex2) {
-
+    public static int calculateABS(String hex1, String hex2) {
         Color hex1Color = Color.decode(hex1);
         Color hex2Color = Color.decode(hex2);
 
+        return Math.abs(hex1Color.getRed() - hex2Color.getRed()) + Math.abs(hex1Color.getGreen() - hex2Color.getGreen()) + Math.abs(hex1Color.getBlue() - hex2Color.getBlue());
+
+    }
+
+    public static double calculateDeltaE76(String hex1, String hex2) {
+
         float[] hex1Normalized = {
-                hex1Color.getRed() / 255.0f,
-                hex1Color.getGreen() / 255.0f,
-                hex1Color.getBlue() / 255.0f
+                Color.decode(hex1).getRed() / 255.0f,
+                Color.decode(hex1).getGreen() / 255.0f,
+                Color.decode(hex1).getBlue() / 255.0f
         };
 
         float[] hex2Normalized = {
-                hex2Color.getRed() / 255.0f,
-                hex2Color.getGreen() / 255.0f,
-                hex2Color.getBlue() / 255.0f
+                Color.decode(hex2).getRed() / 255.0f,
+                Color.decode(hex2).getGreen() / 255.0f,
+                Color.decode(hex2).getBlue() / 255.0f
         };
 
         //TODO: here
